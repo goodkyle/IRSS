bf5d573ff1f8884f186d4458efbaca94ef743280
heavens <zhengpeiyong@gmail.com>
2018-06-25 09:29:25 +0800

    update file commit.

--- demo/demo.go
+++ demo/demo.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
+
 
 	fmt.Println("I'm a demo,hahaha!")
 	fmt.Println("commit version v1.1.  hahaha")
-=======
-	fmt.Println("hahahahaha")
->>>>>>> 90483535444d8c29d7e781e7d44c79fc20907a98
+	fmt.Println("hahahahaha--1")
 
 }

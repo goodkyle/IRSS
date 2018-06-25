aa78ed6ab080dfa92a2ea6b0ef0f2ee9fd7c8691
heavens <zhengpeiyong@gmail.com>
2018-06-25 15:04:15 +0800

    update file commit.

--- demo/demo.go
+++ demo/demo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/xormplus/xorm"
 )
 
 func main() {
@@ -9,8 +10,30 @@ func main() {
 
 	fmt.Println("I'm a demo,hahaha!")
 	fmt.Println("commit version v1.1.  hahaha")
+
+	TestDB()
+
 	fmt.Println("hahahahaha--1234")
 
 }
 
 
+func TestDB() {
+
+	//数据库连接参数
+	params := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true", "root", "1qaz2wsx", "192.168.2.193:3306", "irss")
+
+	//连接数据库
+	engine, err := xorm.NewEngine("mysql",params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("-----1---------")
+	err = engine.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("-----2---------")
+}

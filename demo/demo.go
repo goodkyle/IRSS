cdaccd1d00a957bc163ea575b41b741a1079dc70
heavens <zhengpeiyong@gmail.com>
2018-06-26 17:28:38 +0800

    update file commit.

--- demo/demo.go
+++ demo/demo.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/xormplus/xorm"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/goodkyle/irss/models"
+	"github.com/xormplus/xorm"
 )
 
 var engine *xorm.Engine
@@ -20,14 +20,13 @@ func main() {
 
 }
 
-
 func TestDB() {
 
 	//数据库连接参数定义
 	params := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "root", "1qaz2wsx", "192.168.2.193:3306", "irss")
 
 	//创建ORM引擎
-	engine, err = xorm.NewEngine("mysql",params)
+	engine, err = xorm.NewEngine("mysql", params)
 	if err != nil {
 		panic(err)
 		return
@@ -42,25 +41,98 @@ func TestDB() {
 	//控制台打印出SQL日志
 	engine.ShowSQL(true)
 
-	//设置连接池的空闲数大小
-	//engine.SetMaxIdleConns(5)
+	Query3()
 
-	//设置最大打开连接数
-	//engine.SetMaxOpenConns(5)
-
-	//直接运行sql操作数据库
-	//results, err := engine.QueryValue("select * from r_factors rf where rf.factor_name = ? ", "高血压")
-
-	//使用ORM操作数据库
-	rf := new(models.RFactors)
-	rf.FactorName = "高血压"
-
-	results, err := engine.QueryValue()
-
-	fmt.Println("---------1---------", results)
 	err = engine.Close()
 	if err != nil {
 		panic(err)
+	}
+
+}
+
+//使用Rows遍历查询多条记录
+func Query1() {
+
+	rows, err := engine.Rows(&models.RElement{FactorId: 2})
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	defer rows.Close()
+	re := new(models.RElement)
+
+	for i := 0; rows.Next(); i++ {
+		err = rows.Scan(re)
+		fmt.Printf("re[%d].FactorId = %d \n", i, re.FactorId)
+		fmt.Printf("re[%d].ElementId = %d \n", i, re.ElementId)
+		fmt.Printf("re[%d].ElementName = %s \n", i, re.ElementName)
+	}
+}
+
+//直接执行sql查询多条记录
+func Query2() {
+
+	var re []models.RElement
+
+	err := engine.SQL("select * from r_element re where re.FACTOR_ID = ?", 2).Find(&re)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	for i := 0; i < len(re); i++ {
+		fmt.Printf("re[%d].FactorId = %d \n", i, re[i].FactorId)
+		fmt.Printf("re[%d].ElementId = %d \n", i, re[i].ElementId)
+		fmt.Printf("re[%d].ElementName = %s \n", i, re[i].ElementName)
+	}
+}
+
+func Query3()  {
+
+	type ResultMap struct {
+		FactorName         string
+		FactorRelationName string
+		SubFactorName      string
+		ParentElementId    int
+		ElementId          string
+		ProductTypeName    string
+		ScoreTypeName      string
+		ElementScore       float64
+	}
+
+	sql := "SELECT rf.FACTOR_NAME AS FACTOR_NAME," +
+		"bfr.FACTOR_RELATION_NAME AS FACTOR_RELATION_NAME," +
+		"rf2.FACTOR_NAME AS SUB_FACTOR_NAME," +
+		"re.PARENT_ELEMENT_ID AS PARENT_ELEMENT_ID," +
+		"re.ELEMENT_ID AS ELEMENT_ID," +
+		"re.ELEMENT_NAME AS ELEMENT_NAME," +
+		"bpt.PRODUCT_TYPE_NAME AS PRODUCT_TYPE_NAME," +
+		"bst.SCORE_TYPE_NAME AS SCORE_TYPE_NAME," +
+		"res.ELEMENT_SCORE AS ELEMENT_SCORE " +
+		"FROM r_factors rf " +
+		"LEFT JOIN m_factors mf ON mf.FACTOR_ID = rf.FACTOR_ID " +
+		"LEFT JOIN ( SELECT rf.FACTOR_ID, rf.FACTOR_NAME FROM r_factors rf, m_factors mf WHERE rf.FACTOR_ID = mf.RELATED_FACTOR_ID ) rf2 ON rf2.FACTOR_ID = mf.RELATED_FACTOR_ID " +
+		"LEFT JOIN b_factor_relation bfr ON mf.FACTOR_RELATION_ID = bfr.FACTOR_RELATION_ID " +
+		"LEFT JOIN r_element re ON re.FACTOR_ID = rf2.FACTOR_ID " +
+		"LEFT JOIN r_element_score res ON res.ELEMENT_ID = re.ELEMENT_ID " +
+		"LEFT JOIN b_product_type bpt ON bpt.PRODUCT_TYPE_ID = re.PRODUCT_TYPE_ID " +
+		"LEFT JOIN b_score_type bst ON bst.SCORE_TYPE_ID = res.SCORE_TYPE_ID " +
+		"WHERE " +
+		"rf.FACTOR_NAME = \"高血压\" " +
+		"AND rf2.FACTOR_NAME IN ( \"冠心病\", \"恶性高血压\" ) " +
+		"AND bpt.PRODUCT_TYPE_NAME = \"寿险\" "
+
+	var rmap []ResultMap
+	err := engine.SQL(sql).Find(&rmap)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	for i := 0; i < len(rmap); i++ {
+
+		fmt.Printf("rmap[%d] : %s  |  %s  |  %s  |  %d  |  %s  |  %s  |  %s  |  %.1f  | \n", i, rmap[i].FactorName, rmap[i].FactorRelationName, rmap[i].SubFactorName, rmap[i].ParentElementId, rmap[i].ElementId, rmap[i].ProductTypeName, rmap[i].ScoreTypeName, rmap[i].ElementScore)
 	}
 
 }
